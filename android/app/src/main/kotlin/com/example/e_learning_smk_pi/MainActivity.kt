330f2e6ace3755650fc1c073d06f87751f5a3663
Badru2 <hilalbadru4@gmail.com>
2023-01-26 13:44:05 +0700

    awalan

---
+++ android/app/src/main/kotlin/com/example/e_learning_smk_pi/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.e_learning_smk_pi
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/screens/home_screen.dart
@@ -0,0 +1,16 @@
+import 'package:e_learning_smk_pi/widget/password.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter/src/widgets/container.dart';
+import 'package:flutter/src/widgets/framework.dart';
+
+class HomeScreen extends StatelessWidget {
+  const HomeScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(),
+      body: const PasswordLogin(),
+    );
+  }
+}
