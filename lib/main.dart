98524712d67633f4a3700a7152117944d4d07ab4
Badru2 <hilalbadru4@gmail.com>
2023-01-30 10:42:04 +0700

    benerin password

--- lib/main.dart
+++ lib/main.dart
@@ -9,6 +9,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return const MaterialApp(
+      debugShowCheckedModeBanner: false,
       title: 'E Learning SMK PI',
       home: LoginScreen(),
     );
--- lib/widgets/passwordlogin.dart
+++ lib/widgets/passwordlogin.dart
@@ -1,22 +1,40 @@
 import 'package:flutter/material.dart';
 
-class Password1 extends StatelessWidget {
+class Password1 extends StatefulWidget {
   const Password1({super.key});
+
+  @override
+  State<Password1> createState() => _Password1State();
+}
+
+class _Password1State extends State<Password1> {
+  var _sembunyi = true;
+
+  @override
+  void iniState() {
+    super.initState();
+  }
 
   @override
   Widget build(BuildContext context) {
     return Column(
       children: [
         TextFormField(
-          obscureText: true,
+          obscureText: _sembunyi,
           onChanged: ((value) {}),
           obscuringCharacter: ".",
           decoration: InputDecoration(
             border: OutlineInputBorder(borderRadius: BorderRadius.circular(10)),
             hintText: 'Password',
             suffixIcon: IconButton(
-              icon: const Icon(Icons.remove_red_eye),
-              onPressed: () {},
+              icon: _sembunyi
+                  ? const Icon(Icons.visibility)
+                  : const Icon(Icons.visibility_off),
+              onPressed: () {
+                setState(() {
+                  _sembunyi = !_sembunyi;
+                });
+              },
             ),
           ),
         ),
@@ -24,3 +42,5 @@ class Password1 extends StatelessWidget {
     );
   }
 }
+
+class SetState {}
